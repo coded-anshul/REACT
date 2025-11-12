bf337242290956e6a85adb5e125dadad165b7764
coded-anshul <anshuljain2106@gmail.com>
2025-11-12 12:26:30 +0530

    import export

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { memo } from 'react';
+
+const aj="holaa";
+const dj="lola";
 
 
+const App = () => {
+  return(
+    <div>
+      <h2>hello</h2>
+    </div>
+  );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App;
+export {aj , dj};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import {aj} from './App.jsx'
+import{dj} from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+console.log(aj,dj);
+createRoot(document.getElementById('root')).render(<App />)
